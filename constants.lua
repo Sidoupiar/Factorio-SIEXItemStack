ec6900ec176acc3c11dba245dfff3bf8a2506abc
Sidoupiar <sidoupiar@qq.com>
2020-11-25 10:35:39 +0800

    兼容性

--- constants.lua
+++ constants.lua
@@ -5,8 +5,8 @@ local data =
 	{
 		enable_param = { "bool" , "startup" , true } ,
 		during_tool = { "double" , "startup" , 1 , 0 , 1000000000 } ,
-		during_tool_r = { "double" , "startup" , 1 , 0 , 1000000000 } ,
-		speed_tool_r = { "double" , "startup" , 1 , 0 , 1000000000 } ,
+		during_tool_repair = { "double" , "startup" , 1 , 0 , 1000000000 } ,
+		speed_tool_repair = { "double" , "startup" , 1 , 0 , 1000000000 } ,
 		speed_lab = { "double" , "startup" , 1 , 0 , 1000000000 } ,
 		magazine_ammo = { "double" , "startup" , 1 , 0 , 1000000000 } ,
 		distance_circuit = { "double" , "startup" , 1 , 0 , 1000000000 } ,
@@ -14,11 +14,11 @@ local data =
 		distance_undergrund_pipe = { "double" , "startup" , 1 , 0 , 1000000000 } ,
 	}
 }
-for key , value in pairs{ mult = 1 , size = 0 } do
+for key , value in pairs{ mult = { 1 , 0 } , size = { 0 , -1000000000 } } do
 	data.settings["enable_"..key] = { "bool" , "startup" , true }
 	data.settings[key.."_min"] = { "int" , "startup" , 0 , 0 , 1000000000 }
 	data.settings[key.."_max"] = { "int" , "startup" , 0 , 0 , 1000000000 }
-	for name , type in pairs( SITypes.stackableItem ) do data.settings[key.."_"..type] = { "double" , "startup" , value , 0 , 1000000000 } end
+	for name , type in pairs( SITypes.stackableItem ) do data.settings[key.."_"..type] = { "double" , "startup" , value[1] , value[2] , 1000000000 } end
 end
 
 return data
